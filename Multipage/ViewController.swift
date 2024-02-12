5c6c35553fa9bb13f4c8fea12b7bb17c078aa5c2
Susruthad <susruthanewid@gmail.com>
2024-02-12 10:56:28 -0800

    Initial Commit

---
+++ Multipage/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Multipage
+//
+//  Created by user240733 on 2/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
